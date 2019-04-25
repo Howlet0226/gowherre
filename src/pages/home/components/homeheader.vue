1e57a7861074d2fc7c3eeed3e5fd46cb4882c658
howlet <825111441@qq.com>
2019-04-25 10:53:40 +0800

    add header

---
+++ src/pages/home/components/homeheader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header">
+    <div class="haeder-left">
+      <span class="iconfont back-icon">&#xeb99;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe623;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homeheader"
+};
+</script>
+<style scoped lang='less'>
+@import "~styles/varibles.less";
+.header {
+  display: flex;
+  line-height: @headerHeight;
+  background: @bgColor;
+  color: #fff;
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+    .header-input {
+      // 让此内容占据header盒子中剩下的部分
+      flex: 1;
+      height: 0.64rem;
+      line-height: 0.64rem;
+      margin-top: 0.12rem;
+      margin-left: 0.2rem;
+      padding-left: 0.2rem;
+      background: #fff;
+      border-radius: 0.1rem;
+      color: #ccc;
+    }
+    .header-right {
+      min-width: 1.04rem;
+      padding: 0 0.1rem;
+      float: right;
+      text-align: center;
+      color: #fff;
+      .arrow-icon {
+        margin-left: -0.04rem;
+        font-size: 0.24rem;
+      }
+    }
+  }
+
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <homeheader />
+  </div>
+</template>
+
+<script>
+import homeheader from './components/homeheader'
+export default {
+  name:'home',
+  components:{
+    homeheader
+  }
+}
+
+</script>
+<style scoped>
+</style>
