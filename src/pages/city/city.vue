a0a5cf6239515f098c8849de91d790350732ffa4
howlet <825111441@qq.com>
2019-05-06 22:24:46 +0800

    add city header

---
+++ src/pages/city/city.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <cityHeader />
+  </div>
+</template>
+
+<script>
+    import cityHeader from './components/cityHeader'
+export default {
+    name:'city',
+    components:{
+       cityHeader 
+    }
+}
+
+</script>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/pages/home/home.vue'
+import city from '@/pages/city/city.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/city',
+      name: 'city',
+      component: city
     }
   ]
 })
